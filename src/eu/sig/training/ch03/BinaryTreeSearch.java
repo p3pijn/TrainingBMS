5ece118be2e569cc4e58c53507d36e7a2b2d7768
Pepijn van de Kamp <p.vandekamp@sig.eu>
2016-11-21 15:55:15 +0100

    Renamed vars in ch03

--- src/eu/sig/training/ch03/BinaryTreeSearch.java
+++ src/eu/sig/training/ch03/BinaryTreeSearch.java
@@ -4,26 +4,29 @@ import eu.sig.training.ch03.TreeException;
 import eu.sig.training.ch03.BinaryTreeNode;
 
 public class BinaryTreeSearch {
-
+	
     // tag::calculateDepth[]
-    public static int calculateDepth(BinaryTreeNode<Integer> t, int n) {
-        int depth = 0;
-        if (t.getValue() == n) {
+    public static int calculateDepth(BinaryTreeNode<Integer> node, int nodeValue) {
+        if(node == null){
+        	throw new IllegalArgumentException("A reference to argument 'node' was not specified");
+        }
+    	int depth = 0;
+        if (node.getValue() == nodeValue) {
             return depth;
         } else {
-            if (n < t.getValue()) {
-                BinaryTreeNode<Integer> left = t.getLeft();
+            if (nodeValue < node.getValue()) {
+                BinaryTreeNode<Integer> left = node.getLeft();
                 if (left == null) {
                     throw new TreeException("Value not found in tree!");
                 } else {
-                    return 1 + calculateDepth(left, n);
+                    return 1 + calculateDepth(left, nodeValue);
                 }
             } else {
-                BinaryTreeNode<Integer> right = t.getRight();
+                BinaryTreeNode<Integer> right = node.getRight();
                 if (right == null) {
                     throw new TreeException("Value not found in tree!");
                 } else {
-                    return 1 + calculateDepth(right, n);
+                    return 1 + calculateDepth(right, nodeValue);
                 }
             }
         }
