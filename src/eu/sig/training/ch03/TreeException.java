0392ae2d3432a7e3ebb7ac36870da151021e5df8
Pepijn van de Kamp <p.vandekamp@sig.eu>
2016-11-21 15:28:14 +0100

    Chapter 3

---
+++ src/eu/sig/training/ch03/TreeException.java
@@ -0,0 +1,8 @@
+package eu.sig.training.ch03;
+
+@SuppressWarnings("serial")
+public class TreeException extends RuntimeException {
+    public TreeException(String msg) {
+        super(msg);
+    }
+}
